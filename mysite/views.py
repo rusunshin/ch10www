c815de414ae590dc1a8b4b37d5b8483bb1859e46
CJCU-CC <108b08883@mailst.cjcu.edu.tw>
2021-05-11 09:40:02 +0800

    commit

---
+++ mysite/views.py
@@ -0,0 +1,130 @@
+from django.shortcuts import render
+import json
+import urllib
+from django.conf import settings
+from . import models, forms
+from django.core.mail import send_mail
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect
+# Create your views here.
+# def index(request):
+#     years = range(1960,2021)
+#     try:
+#         urid = request.GET['user_id']
+#         urpass = request.GET['user_pass']
+#         uryear = request.GET['byear']
+#         urfcolor = request.GET.getlist('fcolor')
+#     except:
+#         urid = None
+
+#     if urid != None and urpass == '12345':
+#         verified = True
+#     else:
+#         verifeid = False
+#     return render(request, 'index.html', locals())
+
+def index(request, pid=None, del_pass=None):
+    if 'username' in request.session:
+        username = request.session['username']
+        usercolor = request.session['usercolor']
+
+    return render(request, 'index.html', locals())
+
+def listing(request):
+    posts = models.Post.objects.filter(enabled=True).order_by('-pub_time')[:150]
+    moods = models.Mood.objects.all()
+    return render(request, 'listing.html', locals())
+
+
+def posting(request):
+    moods = models.Mood.objects.all()
+    message = '如要張貼訊息，則每一個欄位都要填...'
+    return render(request, 'posting.html', locals())
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = forms.ContactForm(request.POST)
+        if form.is_valid():
+            message = "感謝您的來信。"
+            user_name = form.cleaned_data['user_name']
+            user_city = form.cleaned_data['user_city']
+            user_school = form.cleaned_data['user_school']
+            user_email  = form.cleaned_data['user_email']
+            user_message = form.cleaned_data['user_message']
+            mail_body = u'''
+網友姓名：{}
+居住城市：{}
+是否在學：{}
+反應意見：如下
+{}'''.format(user_name, user_city, user_school, user_message)
+            send_mail(
+    '不吐不快',
+    mail_body,
+    'a6898208@gmail.com',
+    [user_email],
+    fail_silently=False,
+)
+
+        else:
+            message = "請檢查您輸入的資訊是否正確！"
+    else:
+        form = forms.ContactForm()
+    return render(request, 'contact.html', locals())
+
+
+
+def post2db(request):
+    if request.method == 'POST':
+        post_form = forms.PostForm(request.POST)
+        if post_form.is_valid():
+            ''' Begin reCAPTCHA validation '''
+            recaptcha_response = request.POST.get('g-recaptcha-response')
+            url = 'https://www.google.com/recaptcha/api/siteverify'
+            values = {
+                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                'response': recaptcha_response
+            }
+            data = urllib.parse.urlencode(values).encode()
+            req =  urllib.request.Request(url, data=data)
+            response = urllib.request.urlopen(req)
+            result = json.loads(response.read().decode())
+            ''' End reCAPTCHA validation '''
+            if result['success']:
+                message = "您的訊息已儲存，要等管理者啟用後才看得到喔。"
+                post_form.save()
+                return HttpResponseRedirect('/list/')
+            else:
+                message = "Invalid reCAPTCHA. Please try again."
+        else:
+            message = '如要張貼訊息，則每一個欄位都要填...'
+    else:
+        post_form = forms.PostForm()
+        message = '如要張貼訊息，則每一個欄位都要填...'          
+
+    return render(request, 'post2db.html', locals())
+
+
+def login(request):
+    if request.method == 'POST':
+        login_form = forms.LoginForm(request.POST)
+        if login_form.is_valid():
+            username = request.POST['user_name']
+            usercolor = request.POST['user_color']
+            message = "登入成功"
+        else:
+            message = "請檢查輸入的欄位內容"
+    else:
+        login_form = forms.LoginForm()
+
+    try:
+        if username: request.session['username'] = username
+        if usercolor: request.session['usercolor'] = usercolor
+    except:
+        pass
+
+    return render(request, 'login.html', locals())
+
+def logout(request):
+    request.session['username'] = None
+    return redirect('/')
